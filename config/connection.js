866809f3da8e8f4f6126d8b1010cf598fe79a8fe
Bob Brown <robertbernardbrown@gmail.com>
2018-03-07 17:58:11 -0500

    adds cleardb credentials

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql");
 const pool  = mysql.createPool({
-  host     : "localhost",
-  user     : "root",
-  password : "root",
+  host     : "us-cdbr-iron-east-05.cleardb.net",
+  user     : "bbbc852cea0568",
+  password : "c6f9f3df",
   database : "restaurants_db",
   port: 3306
 });
