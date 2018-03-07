5d919dd43965e0251c5e61ef0fc4cee3010c6227
Bob Brown <robertbernardbrown@gmail.com>
2018-03-07 18:10:11 -0500

    adds a few more seeds to seed file

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,7 @@ INSERT INTO restaurants (restaurant, visited)
 VALUES 
 ("Llama Inn", 0),
 ("Beco", 0),
-("Vinny's Pizza", 0);
+("Vinny's Pizza", 0),
+("Cafe Collete", 1),
+("The Wythe", 1),
+("Williamsburg Pizza", 1);
