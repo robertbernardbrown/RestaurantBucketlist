c5dd5885ac01b0f5fda7360d7e7082319c9853d0
Bob Brown <robertbernardbrown@gmail.com>
2018-03-07 18:43:45 -0500

    adds correct db name

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const pool  = mysql.createPool({
   host     : "us-cdbr-iron-east-05.cleardb.net",
   user     : "bbbc852cea0568",
   password : "c6f9f3df",
-  database : "restaurants_db",
+  database : "heroku_c7888bcad178b88",
   port: 3306
 });
 
