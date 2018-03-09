67325c39e8b656d7f41aa0e90d5923496584785f
Bob Brown <robertbernardbrown@gmail.com>
2018-03-09 18:30:48 -0500

    comments out code for now while developing registration. Sets index.handlebars to default to registration page

--- controllers/restaurant_controller.js
+++ controllers/restaurant_controller.js
@@ -3,40 +3,52 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  restaurant.all(data => {
-    let hbsObject = {
-      restaurant: data
-    };
-    res.render("index", hbsObject);
-  });
+  res.render("index");
 });
 
-router.post("/", (req, res) => {
-  restaurant.create(req.body.restaurant, data => {
-    res.json({ id: data.insertId });
-  });
+router.post("/register", (req, res) => {
+
+  console.log(req.body.username);
+  console.log(req.body.password);
+
+  res.render("index");
 });
 
-router.put("/:id", (req, res) => {
-  var condition = req.params.id;
-  restaurant.update(req.body.visited, condition, data => {
-    if (data.changedRows === 0) {
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
+// router.get("/", (req, res) => {
+//   restaurant.all(data => {
+//     let hbsObject = {
+//       restaurant: data
+//     };
+//     res.render("index", hbsObject);
+//   });
+// });
 
-router.delete("/:id", (req, res) => {
-  var condition = req.params.id;
-  restaurant.delete( condition, data => {
-    if (data.alteredRows === 0) {
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
+// router.post("/", (req, res) => {
+//   restaurant.create(req.body.restaurant, data => {
+//     res.json({ id: data.insertId });
+//   });
+// });
+
+// router.put("/:id", (req, res) => {
+//   var condition = req.params.id;
+//   restaurant.update(req.body.visited, condition, data => {
+//     if (data.changedRows === 0) {
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
+
+// router.delete("/:id", (req, res) => {
+//   var condition = req.params.id;
+//   restaurant.delete( condition, data => {
+//     if (data.alteredRows === 0) {
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
 
 module.exports = router;
