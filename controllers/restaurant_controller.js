b0d724e8f0413d09bc850fbd3680b63d2d67ea7a
Bob Brown <robertbernardbrown@gmail.com>
2018-03-10 19:33:27 -0500

    adds express-validator to project and adds some code to validate usernames and passwords

--- controllers/restaurant_controller.js
+++ controllers/restaurant_controller.js
@@ -1,14 +1,34 @@
-const restaurant = require("../models/restaurant");
-const express = require("express");
-const router = express.Router();
+const restaurant                  = require("../models/restaurant");
+const express                     = require("express");
+const router                      = express.Router();
+const { check, oneOf,validationResult } = require("express-validator/check");
+const { matchedData, sanitize }   = require("express-validator/filter");
 
 router.get("/register", (req, res) => {
   res.render("register");
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", oneOf([
+  [
+    check("username", "Username field can't be empty.")
+      .exists()
+      .custom((value) => value !== ""),
+    check("password", "Password field can't be empty.")
+      .exists()
+      .custom((value) => value !== ""),
+    check("password2", "Password Confirmation field must match password field")
+      .exists()
+      .custom((value, { req }) => value === req.body.password)
+  ],
+]),(req, res) => {
   let username = req.body.username;
   let password = req.body.password;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.mapped() });
+  }
+
   console.log(username, password);
   restaurant.auth(username, password, (data) => {
     console.log(data);
