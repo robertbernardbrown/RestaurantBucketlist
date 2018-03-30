406c436a99f01130664f2502a2bb2c9d9ddb1fe0
Bob Brown <robertbernardbrown@gmail.com>
2018-03-30 17:21:07 -0400

    adjusts nightmare

--- test/nightmare.js
+++ test/nightmare.js
@@ -4,19 +4,37 @@ const nightmare = Nightmare({ show: true });
 nightmare
   .goto("https://restaurant-bucketlist.herokuapp.com/")
   .click("#register")
-  .type("#exampleUsername1", "Nightmare6")
+  .type("#exampleUsername1", "Nightmare10")
   .type("#exampleInputPassword1", "lol")
   .type("#exampleInputPassword2", "lol")
   .click("#submit")
   .wait("#rest")
   .type("#rest", "McNaldo's")
-  .click(".submit-btn")//
-  .wait("button.change-visited")
-  .click("button.change-visited")
-  .evaluate(() => document.querySelector("#row").href)
-  .click("button.delete-rest")
+  .click(".submit-btn")
+  // .wait("#label")
+  .evaluate(() => document.querySelector("#label").href)
   .end()
   .then(console.log)
   .catch(error => {
     console.error("Search failed:", error);
   });
+
+// nightmare
+//   .goto("https://restaurant-bucketlist.herokuapp.com/")
+//   .click("#register")
+//   .type("#exampleUsername1", "Nightmare6")
+//   .type("#exampleInputPassword1", "lol")
+//   .type("#exampleInputPassword2", "lol")
+//   .click("#submit")
+//   .wait("#rest")
+//   .type("#rest", "McNaldo's")
+//   .click(".submit-btn")
+//   .wait("button.change-visited")
+//   .click("button.change-visited")
+//   .evaluate(() => document.querySelector("#row").href)
+//   .click("button.delete-rest")
+//   .end()
+//   .then(console.log)
+//   .catch(error => {
+//     console.error("Search failed:", error);
+//   });
